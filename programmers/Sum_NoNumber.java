1183fc57d0861a73ef84b6798ab3cc3d9200b5b7
woorim05 <84488846+woorim05@users.noreply.github.com>
2021-10-06 22:03:05 +0900

    Create Sum_NoNumber.java

---
+++ programmers/Sum_NoNumber.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        for (int i = 0; i <= 9; i++) {
+            answer += contain(numbers, i);
+        }
+        return answer;
+    }
+
+    public static int contain(int[] numbers, int input_data) {
+        for (int num : numbers) {
+            if (num == input_data)
+                return 0;
+        }
+        return input_data;
+   }
+}
