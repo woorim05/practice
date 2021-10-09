fa8d23a54e0bf48320131dc652db8dd2ac2a7cf0
woorim05 <84488846+woorim05@users.noreply.github.com>
2021-10-09 22:48:09 +0900

    Create Combination.java

---
+++ programmers/Combination.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.Map.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        for (String[] str : clothes)
+            hm.put(str[1], hm.getOrDefault(str[1], 0) + 1);
+
+        for (Entry<String, Integer> entry : hm.entrySet()) 
+            answer *= entry.getValue() + 1;
+
+        answer--;
+
+        return answer;
+    }
+}
