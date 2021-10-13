5ea207278640343b5af492ded628b6c5e31a9ed4
woorim05 <84488846+woorim05@users.noreply.github.com>
2021-10-13 20:07:43 +0900

    Create FunctionDev.java

---
+++ programmers/FunctionDev.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+// 1차 시도 배열, 리스트 사용
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] days = new int[speeds.length];
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < speeds.length; i++) {
+            days[i] = (int) Math.ceil((100-progresses[i]) / speeds[i]);
+        }
+        
+        for (int i = 0; i < days.length; i++) {
+            int count = 1;
+            if (days[i] == 0) continue;
+           
+            for (int j = i+1; j < days.length; j++) {
+                if (days[i] >= days[j]) {
+                    count++;
+                    days[j] = 0;
+                } else break;
+            }
+            
+            list.add(count);
+        }
+        
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
