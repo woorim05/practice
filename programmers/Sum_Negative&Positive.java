17019e5ffd8eb05358a428020f09d24e857d9578
woorim05 <84488846+woorim05@users.noreply.github.com>
2021-10-16 06:51:44 +0900

    음양 더하기
    정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와
    이 정수들의 부호를 차례대로 담은 boolean 배열 signs(true = 양수, false = 음수)가 매개변수로 주어질 때,
    실제 정수들의 합

--- programmers/Sum_Negative&Positive.java
+++ programmers/Sum_Negative&Positive.java
@@ -1,14 +1,22 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-
         for (int i = 0; i < absolutes.length; i++) {
             if (signs[i]) 
                 answer += absolutes[i];
             else
                 answer -= absolutes[i];
         }
+        return answer;
+    }
+}
 
+// 삼항 연산자 사용
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i=0; i<signs.length; i++)
+            answer += absolutes[i] * (signs[i]? 1: -1);
         return answer;
     }
 }
