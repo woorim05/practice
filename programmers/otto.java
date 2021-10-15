83af8072d93304e90d9959a9647369dd60383fc1
woorim05 <84488846+woorim05@users.noreply.github.com>
2021-10-16 06:59:47 +0900

    로또의 최고 순위와 최저 순위
    로또 번호를 담은 배열 lottos, 당첨 번호를 담은 배열 win_nums가 매개변수로 주어질 때,
    당첨 가능한 최고 순위와 최저 순위를 차례대로 담은 배열 구하기

--- programmers/otto.java
+++ programmers/otto.java
@@ -1,6 +1,6 @@
-public class Solution {
-	public int[] solution(int[] lottos, int[] win_nums) {
-		int[] answer = new int[2];
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
 		int[] rank = {6,6,5,4,3,2,1};
 		int min_score = 0;
 		int unknown = 0;
@@ -21,6 +21,6 @@ public class Solution {
 		answer[0] = rank[min_score + unknown];
 		answer[1] = rank[min_score];
 
-		return answer;
-	}
+        return answer;
+    }
 }
