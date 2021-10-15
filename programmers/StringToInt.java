233e19a83016bb43091eb9fa3c5b3c63a4c1c8ab
woorim05 <84488846+woorim05@users.noreply.github.com>
2021-10-16 06:47:29 +0900

    숫자 문자열과 영단어
    숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 혹은 바뀌지 않고 그대로인 문자열 s가 매개변수로 주어질 때, s가 의미하는 원래 숫자 구하기

--- programmers/StringToInt.java
+++ programmers/StringToInt.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution { 
     public int solution(String s) {
         int answer = 0;
         String[] alphabet = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
