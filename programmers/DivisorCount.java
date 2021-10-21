c9d2b18d72c115141a4e02125ef754ca17672e88
woorim05 <84488846+woorim05@users.noreply.github.com>
2021-10-21 20:56:52 +0900

    약수의 개수와 덧셈
    두 정수 left와 right가 매개변수로 주어질 때,
    left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수

---
+++ programmers/DivisorCount.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        
+        for (int i = left; i <= right; i++)
+            answer += divisor(i);
+        
+        return answer;
+    }
+
+    public int divisor(int num) {
+        int cnt = 1;
+        
+        for (int i = 1; i < num; i++) 
+            if (num%i == 0) cnt++;
+        
+        if (cnt%2 == 0) return num;
+        else return -num;
+    }
+}
