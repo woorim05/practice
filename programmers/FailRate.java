55bb4d06e7933798508a6b93037a8e8f0765c243
woorim05 <84488846+woorim05@users.noreply.github.com>
2021-10-23 09:48:43 +0900

    실패율
    전체 스테이지의 개수 N, 게임을 이용하는 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열 stages가 매개변수로 주어질 때,
    실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 return

--- programmers/FailRate.java
+++ programmers/FailRate.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+// 첫 번째 방법 : Map에 모든 정보 담기
 class Solution {
     public int[] solution(int N, int[] stages) {
         int[] answer = new int[N];
@@ -40,6 +40,38 @@ class Solution {
         for(Map.Entry<Integer, Double> entry : list){
             answer[cnt] = entry.getKey();
             cnt++;
+        }
+        
+        
+        return answer;
+    }
+}
+
+
+// 두 번째 방법 : 변수 사용한 후 바로 Map에 담기
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        Map<Integer, Double> failRate = new HashMap<>();  // <스테이지, 실패율>
+        
+        for (int i = 1; i <= N; i++) {
+            int all = 0;
+            int fail = 0;
+            
+            for (int j = 0; j < stages.length; j++) {
+                if (stages[j] == i) fail++;
+                if (stages[j] >= i) all++;
+            }
+
+            if (fail == 0 || all == 0) failRate.put(i, (double) 0.0);
+            else failRate.put(i, (double) fail/all);
+        }
+                
+        List<Map.Entry<Integer, Double>> list = new ArrayList<>(failRate.entrySet());
+        list.sort((o1, o2) -> Double.compare(o2.getValue(), o1.getValue()));
+        
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i).getKey();
         }
         
         
