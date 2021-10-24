fe4ae7d83a3cfa3861c195e153279c1439b2a834
woorim05 <84488846+woorim05@users.noreply.github.com>
2021-10-24 20:42:17 +0900

    다트 게임

--- programmers/DartGame2.java
+++ programmers/DartGame2.java
@@ -6,28 +6,28 @@ class Solution {
         
         for (int i = 0; i < dartResult.length(); i++) {
             char ch = dartResult.charAt(i);
-            if (ch == '1' && dartResult.charAt(i+1) == '0') {
-                score[s] = 10;
-                i++;
-            } else {
-                if (Character.isDisit(ch)) 
-                    score[s] = ch - '0';
-                else if (ch == 'S')
-                    s++;
+            if (Character.isDigit(ch)) { // 숫자
+                score[s] = ch - '0';
+                if (ch == '1' && dartResult.charAt(i+1) == '0') {
+                    score[s] = 10;
+                    i++;
+                }
+            } else { // 문자
+                if (ch == 'S') s++;
                 else if (ch == 'D') {
                     score[s] = (int) Math.pow(score[s], 2);
                     s++;
                 } else if (ch == 'T') {
                     score[s] = (int) Math.pow(score[s], 3);
                     s++;
+                } else if (ch == '#') {
+                    score[s-1] *= -1; 
                 } else if (ch == '*') {
                     if (s == 1) score[0] *= 2;
                     else {
                         score[s-1] *= 2;
                         score[s-2] *= 2;
                     }
-                } else if (ch == '#') {
-                    score[s-1] *= -1; 
                 }
             }
         }
