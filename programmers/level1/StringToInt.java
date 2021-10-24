39457ffa811780174b4e31e9ccc4bff7762d16bd
woorim05 <84488846+woorim05@users.noreply.github.com>
2021-10-24 21:31:37 +0900

    숫자 문자열과 영단어

--- programmers/level1/StringToInt.java
+++ programmers/level1/StringToInt.java
@@ -1,3 +1,8 @@
+/*
+숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 혹은 바뀌지 않고 그대로인 문자열 s가
+매개변수로 주어질 때, s가 의미하는 원래 숫자를 return 하도록 solution 함수를 완성해주세요.
+*/
+
 class Solution { 
     public int solution(String s) {
         int answer = 0;
