95b10d3cee9859dea8eb721022e1247743b7c1fe
woorim05 <84488846+woorim05@users.noreply.github.com>
2021-10-24 21:12:10 +0900

    전화번호 목록

--- programmers/level2/ContainPrefix.java
+++ programmers/level2/ContainPrefix.java
@@ -1,3 +1,9 @@
+/*
+전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때,
+어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록
+solution 함수를 작성해주세요.
+*/
+
 import java.util.*;
 import java.util.Map.*;
 
