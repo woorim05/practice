761fb07bf70e261eac0aff4e5db91d93553d6c00
woorim05 <84488846+woorim05@users.noreply.github.com>
2021-10-24 21:26:34 +0900

    로또의 최고 순위와 최저 순위

--- programmers/level1/Lotto.java
+++ programmers/level1/Lotto.java
@@ -1,3 +1,8 @@
+/*
+민우가 구매한 로또 번호를 담은 배열 lottos, 당첨 번호를 담은 배열 win_nums가 매개변수로 주어질 때,
+당첨 가능한 최고 순위와 최저 순위를 차례대로 배열에 담아서 return 하도록 solution 함수를 완성해주세요.
+*/
+
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
